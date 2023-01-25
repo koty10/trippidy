02aa329d6ea162a999afd75f160626543e7642c6
Daniel Koten <daniel.koten@i-projekt.cz>
2023-01-25 21:20:26 +0100

    Revert "isar - generated models added"
    This reverts commit 46ed52f943da917b5a5e62efe679e968701acdd4.

--- lib/model/item_base_model.dart
+++ lib/model/item_base_model.dart
@@ -1,7 +1,7 @@
 import 'package:anti_forgetter/model/category_model.dart';
 import 'package:anti_forgetter/model/item_model.dart';
 import 'package:isar/isar.dart';
-part 'item_base_model.g.dart';
+part 'item_model.g.dart';
 
 @collection
 class ItemBaseModel {
