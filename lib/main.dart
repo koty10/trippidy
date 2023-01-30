c28aed42ecb888544d7c710a83b285d9d7ef3d3a
Daniel Koten <daniel.koten@i-projekt.cz>
2023-01-30 21:47:11 +0100

    roles added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:trippidy/model/enum/role.dart';
 import 'package:trippidy/model/item.dart';
 import 'package:trippidy/model/member.dart';
 import 'package:trippidy/model/trip.dart';
@@ -7,6 +12,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+import 'firebase_options.dart';
 import 'model/category.dart';
 
 Future<void> main() async {
@@ -17,14 +23,32 @@ Future<void> main() async {
     ..registerAdapter(ItemAdapter())
     ..registerAdapter(MemberAdapter())
     ..registerAdapter(TripAdapter())
-    ..registerAdapter(UserAdapter());
+    ..registerAdapter(UserAdapter())
+    ..registerAdapter(RoleAdapter());
 
+  Hive.deleteBoxFromDisk("trips");
   await Hive.openBox<Trip>('trips');
 
   // DummyDataService().initHiveDb();
 
-  //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  //var db = FirebaseFirestore.instance;
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  var db = FirebaseFirestore.instance;
+  QuerySnapshot snapshot = await db
+      .collection("trips")
+      .where("members", arrayContains: {"id": 2}).get();
+  var docs = snapshot.docs;
+  log(docs.toString());
+
+  QuerySnapshot snapshot2 = await db.collection("trips").get();
+  var docs2 = snapshot2.docs;
+
+  QuerySnapshot snapshot3 =
+      await db.collection("trips").where("members.id", isEqualTo: 2).get();
+  var docs3 = snapshot3.docs;
+
+  QuerySnapshot snapshot4 =
+      await db.collection("trips").where("owner.id", isEqualTo: 1).get();
+  var docs4 = snapshot4.docs;
 
   // Add a new document with a generated ID
   // log(Hive.box<Trip>("trips").toString());
