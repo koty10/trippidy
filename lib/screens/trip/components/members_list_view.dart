6af1577f11c9a423ad00c583036b6c666f9d0bfd
Daniel Koten <daniel.koten@i-projekt.cz>
2023-04-19 23:47:43 +0200

    added lottie and trip.members empty check

--- lib/screens/trip/components/members_list_view.dart
+++ lib/screens/trip/components/members_list_view.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:trippidy/providers/auth_controller.dart';
 
+import '../../../model/member.dart';
 import '../../../model/trip.dart';
 import '../../members_list/members_list_screen.dart';
 import 'member_list_tile.dart';
 
-class MembersListView extends StatelessWidget {
+class MembersListView extends ConsumerWidget {
   final Trip currentTrip;
 
   const MembersListView({super.key, required this.currentTrip});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    List<Member> members = currentTrip.members.where((element) => element.userProfileId != ref.read(authControllerProvider).userProfile!.id).toList();
     return Expanded(
       child: ListView.separated(
         padding: const EdgeInsets.all(16),
-        itemCount: currentTrip.members.length,
+        itemCount: members.length,
         itemBuilder: (BuildContext context, int index) {
-          var curMember = currentTrip.members[index];
+          var curMember = members[index];
           return MemberListTile(
             member: curMember,
             title: "${curMember.userProfileFirstname} ${curMember.userProfileLastname}",
--- lib/screens/trip/trip_screen.dart
+++ lib/screens/trip/trip_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lottie/lottie.dart';
 import 'package:trippidy/model/trip.dart';
 import 'package:trippidy/providers/trip_detail_controller.dart';
 import 'package:trippidy/screens/my_list/my_list_screen.dart';
@@ -7,6 +8,8 @@ import 'package:trippidy/screens/trip/components/member_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:trippidy/screens/trip/components/members_list_view.dart';
 
+import '../../model/member.dart';
+import '../../providers/auth_controller.dart';
 import '../add_member/add_member_screen.dart';
 
 class TripScreen extends ConsumerWidget {
@@ -17,6 +20,7 @@ class TripScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ref) {
     Trip currentTrip = ref.watch(tripDetailControllerProvider);
+    List<Member> members = currentTrip.members.where((element) => element.userProfileId != ref.read(authControllerProvider).userProfile!.id).toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -49,18 +53,34 @@ class TripScreen extends ConsumerWidget {
               ],
             ),
           ),
-          const Padding(
-            padding: EdgeInsets.only(left: 16, top: 8),
-            child: Text(
-              'Seznamy ostatních členů',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 16,
+          if (members.isNotEmpty)
+            const Padding(
+              padding: EdgeInsets.only(left: 16, top: 8),
+              child: Text(
+                'Seznamy ostatních členů',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                ),
               ),
             ),
-          ),
-          MembersListView(currentTrip: currentTrip),
+          members.isNotEmpty
+              ? MembersListView(currentTrip: currentTrip)
+              : Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      LottieBuilder.asset(
+                        'assets/lotties/empty_box.json',
+                        height: 200,
+                      ),
+                      const SizedBox(height: 20),
+                      const Center(child: Text('Žádní členové tu zatím nejsou.')),
+                    ],
+                  ),
+                ),
+          // ).animate().fadeIn(),
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
